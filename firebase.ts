ad3d235297cfb293375a51264208b2afcbf51f79
twistedTongues <flossiedixkeybounce@gmail.com>
2023-09-08 16:16:10 +0300

    feat(app): added authetication and firestore to config file

--- firebase.ts
+++ firebase.ts
@@ -1,3 +1,13 @@
+import { initializeApp } from "firebase/app";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+
+// Optionally import the services that you want to use
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// import {...} from "firebase/database";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBTq85QtydioW6sGREfESt7O2umYjcgPEE",
   authDomain: "signal-clone-302b3.firebaseapp.com",
@@ -6,3 +16,11 @@ const firebaseConfig = {
   messagingSenderId: "157694644467",
   appId: "1:157694644467:web:9417c64064ef3825c56786",
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+const db = getFirestore(app);
+
+export { auth, db };
