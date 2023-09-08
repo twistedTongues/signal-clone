fb260324a219069fdaa4f709c394a868cd31cee6
twistedTongues <flossiedixkeybounce@gmail.com>
2023-09-08 16:17:04 +0300

    feat(app): build login screen

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,22 +1,104 @@
-import { StyleSheet, Text, View } from "react-native";
+import { KeyboardAvoidingView, StyleSheet, View } from "react-native";
 import { Button, Image, Input } from "@rneui/base";
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../App";
 
-const LoginScreen = () => {
+export type NavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "Home"
+>;
+
+type Props = {
+  navigation: NavigationProp;
+};
+
+const LoginScreen = ({ navigation }: Props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      console.log(authUser);
+      if (authUser) {
+        navigation.replace("Home");
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const signIn = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <View>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <StatusBar style="light" />
       <Image
         source={{
           uri:
-            "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+            "https://assets.stickpng.com/images/6002f8aa51c2ec00048c6c68.png",
         }}
         style={{ width: 200, height: 200 }}
       />
-    </View>
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          textContentType="emailAddress"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          textContentType="password"
+          value={password}
+          onChangeText={(pass) => setPassword(pass)}
+          onSubmitEditing={signIn}
+        />
+      </View>
+      <Button
+        containerStyle={styles.button}
+        onPress={signIn}
+        title="Login"
+        radius={5}
+      />
+      <Button
+        containerStyle={styles.button}
+        onPress={() => navigation.navigate("Register")}
+        type="outline"
+        radius={5}
+        title="Register"
+      />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
